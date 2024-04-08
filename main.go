b0a7b17e6fb67d43742736bf5becfc4b1eae7f79
supakorn <supakornboon5039@gmail.com>
2024-04-08 12:34:41 +0700

    feat: start

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	var dayOfWeek = 6
+
+	
+	switch dayOfWeek{
+	case 1:
+		fmt.Println("Monday")
+	case 2:
+		fmt.Println("Tuesday")
+	case 3:
+		fmt.Println("Wednesday")
+	case 4:
+		fmt.Println("Thursday")
+	case 5:
+		fmt.Println("Friday")
+	default:
+		fmt.Println("Invalid Day")
+	}
+}
